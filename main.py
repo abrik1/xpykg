f480692d46b2a162de2b6c73e6495e1e7cb7d1dd
Abhiraj Rik <133798548+abrik1@users.noreply.github.com>
2023-10-21 14:27:17 +0530

    rebrand: xpkg in use so xpykg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 '''
-xpkg: a script to automate install/update/uninstall for Windows XP  
+xpykg: a script to automate install/update/uninstall for Windows XP  
 '''
 
 from json import loads, dumps
@@ -15,24 +15,24 @@ def sync_database():
     '''
     sync_database(): downloads database from github and dumps to Program Files  
     '''
-    if isfile("C:\\Program Files\\xpkg\\db.json") == False:
-        if isdir("C:\\Program Files\\xpkg") == False:
-            mkdir("C:\\Program Files\\xpkg")
+    if isfile("C:\\Program Files\\xpykg\\db.json") == False:
+        if isdir("C:\\Program Files\\xypkg") == False:
+            mkdir("C:\\Program Files\\xypkg")
 
         print("[xpkg:info]: downloading database")
-        with open("C:\\Program Files\\xpkg\\db.json", 'w') as xpkg_db:
+        with open("C:\\Program Files\\xpykg\\db.json", 'w') as xpkg_db:
             try:
-                info = get("https://raw.githubusercontent.com/abrik1/xpkg/main/db.json")
+                info = get("https://raw.githubusercontent.com/abrik1/xpykg/main/db.json")
                 xpkg_db.write(info.content.decode('utf-8'))
                 xpkg_db.close()
             except (MissingSchema, ConnectionError, ConnectionAbortedError, ConnectTimeout, ConnectionRefusedError, ConnectionResetError):
-                print("[xpkg:error]: database failed to download")
+                print("[xpykg:error]: database failed to download")
                 xpkg_db.close()
                 exit(1)
         
-        print("[xpkg:info]: database written")
+        print("[xpykg:info]: database written")
     else:
-        with open("C:\\Program Files\\xpkg\\db.json", 'r+') as xpkg_db:
+        with open("C:\\Program Files\\xpykg\\db.json", 'r+') as xpkg_db:
             try:
                 info = get("https://raw.githubusercontent.com/abrik1/xpkg/main/db.json")
                 if xpkg_db.read() == info.content.decode('utf-8'):
@@ -45,7 +45,7 @@ def sync_database():
                     xpkg_db.close()
                     print("[xpkg:info]: database updated")
             except (MissingSchema, ConnectionError, ConnectionAbortedError, ConnectTimeout, ConnectionRefusedError, ConnectionResetError):
-                print("[xpkg:error]: database failed to download")
+                print("[xpykg:error]: database failed to download")
                 xpkg_db.close()
                 exit(1)
         
@@ -53,19 +53,19 @@ def list_packages():
     '''
     list_packages(): shows the list of packages
     '''
-    if isfile("C:\\Program Files\\xpkg\\db.json") == True:
-        with open("C:\\Program Files\\xpkg\\db.json", 'r') as db:
+    if isfile("C:\\Program Files\\xpykg\\db.json") == True:
+        with open("C:\\Program Files\\xpykg\\db.json", 'r') as db:
             contents = loads(db.read())
             for i in list(contents.keys()):
                 print("{} {}".format(i , contents[i]['version']))
         db.close()
     else:
-        print("[xpkg:error]: package index not found")
+        print("[xpykg:error]: package index not found")
         
 if __name__ == "__main__":
     try:
         if argv[1] in ["-h", "help", "--help"]:
-            print('''xpkg: a script for software management for Windows XP
+            print('''xpykg: a script for software management for Windows XP
 =====================================================
 help: view this page
 install: install <pkg>
@@ -82,9 +82,9 @@ version: show xpkg version''')
         elif argv[1] in ["-l", "list", "--list"]:
             list_packages()
         else:
-            print("[xpkg:error]: invalid argument")
+            print("[xpykg:error]: invalid argument")
     except IndexError:
-        print("[xpkg:error]: invalid argument")
+        print("[xpykg:error]: invalid argument")
     except KeyboardInterrupt:
-        print("[xpkg:error]: keyboard exit detected")
+        print("[xpykg:error]: keyboard exit detected")
         exit(1)
