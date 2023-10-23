8e4bc0f90b0e63c2502cd45cff1683a41d0d906c
Abhiraj Rik <133798548+abrik1@users.noreply.github.com>
2023-10-23 17:10:43 +0530

    add a working install, improve error messages and fix sync

--- main.py
+++ main.py
@@ -1,35 +1,34 @@
 '''
-xpykg: a script to automate install/update/uninstall for Windows XP
+xpykg: a script to automate install/update/uninstall for Windows XP  
 '''
 
 from json import loads, dumps
 from requests import get
 from requests.exceptions import MissingSchema, ConnectionError, ConnectTimeout
 from sys import argv, exit
-from os import system, mkdir
+from os import system, mkdir, getenv
 from os.path import isfile, isdir
 
 xpkg_version = "0.1"
 
 def sync_database():
     '''
-    sync_database(): downloads database from github and dumps to Program Files
+    sync_database(): downloads database from github and dumps to Program Files  
     '''
     if isfile("C:\\Program Files\\xpykg\\db.json") == False:
-        if isdir("C:\\Program Files\\xypkg") == False:
-            mkdir("C:\\Program Files\\xypkg")
+        if isdir("C:\\Program Files\\xpykg") == False:
+            mkdir("C:\\Program Files\\xpykg")
 
-        print("[xpkg:info]: downloading database")
+        print("[xpykg:info]: downloading database")
         with open("C:\\Program Files\\xpykg\\db.json", 'w') as xpkg_db:
             try:
                 info = get("https://raw.githubusercontent.com/abrik1/xpykg/main/db.json")
                 xpkg_db.write(info.content.decode('utf-8'))
                 xpkg_db.close()
             except (MissingSchema, ConnectionError, ConnectionAbortedError, ConnectTimeout, ConnectionRefusedError, ConnectionResetError):
-                print("[xpykg:error]: database failed to download")
+                print("[xpykg:error]: database failed to download.. maybe try again later")
                 xpkg_db.close()
                 exit(1)
-
         print("[xpykg:info]: database written")
     else:
         with open("C:\\Program Files\\xpykg\\db.json", 'r+') as xpkg_db:
@@ -45,10 +44,10 @@ def sync_database():
                     xpkg_db.close()
                     print("[xpkg:info]: database updated")
             except (MissingSchema, ConnectionError, ConnectionAbortedError, ConnectTimeout, ConnectionRefusedError, ConnectionResetError):
-                print("[xpykg:error]: database failed to download")
+                print("[xpykg:error]: database failed to download.. maybe try again later")
                 xpkg_db.close()
                 exit(1)
-
+        
 def list_packages():
     '''
     list_packages(): shows the list of packages
@@ -60,11 +59,11 @@ def list_packages():
                 print("{} {}".format(i , contents[i]['version']))
         db.close()
     else:
-        print("[xpykg:error]: package index not found")
+        print("[xpykg:error]: package index not found.. maybe sync the database to fix it")
 
 def search_packages(query: str):
     '''
-    search_packages(query: str): search packages matching to query
+    search_packages(query: str): search packages matching to query  
     '''
 
     if isfile("C:\\Program Files\\xpykg\\db.json") == True:
@@ -84,11 +83,77 @@ def search_packages(query: str):
                     print("{} {}".format(i, contents[i]['version']))
 
             if count == 0:
-                print("[xpykg:error] no pkgs similar to {}".format(query))    
+                print("[xpykg:error] no pkgs similar to {}".format(query))            
         db.close()
     else:
-        print("[xpykg:error]: database not found")
+        print("[xpykg:error]: database not found.. maybe sync it.")
 
+def install_package(package: str):
+    '''
+    install_package(package): install package to  
+    '''
+
+    if isfile("C:\\Program Files\\xpykg\\db.json") == True:
+        with open("C:\\Program Files\\xpykg\\db.json", 'r') as db:
+            contents = loads(db.read())
+            
+            if package not in list(contents.keys()):
+                print("[xpykg:error] package {} not found in database.. maybe sync database or search it".format(package))
+                db.close()
+                exit(1)
+
+            print("[xpkg:info]: downloading setup.exe for package {}".format(package))
+             
+            try:
+                exe_contents = get(contents[package]['source'])
+            except (MissingSchema, ConnectionError, ConnectionAbortedError, ConnectTimeout, ConnectionRefusedError, ConnectionResetError):
+                print("[xpkg:error]: setup.exe for package {} failed to download.. maybe try again later".format(package))
+                exit(1)
+
+            # Windows executables can either be in .exe or in .msi 
+            # the code below determines the source file is an .exe or a .msi and saves according to it
+
+            if contents[package]['source'].split('.')[len(contents[package]['source'].split('.'))-1] == 'exe':
+                with open("{}\\setup.exe".format(getenv("Temp")), 'wb') as setup:
+                    setup.write(exe_contents.content)
+                setup.close()
+
+                print("[xpykg:note]: running installer for package {}".format(package))
+                if system("{}\\setup.exe".format(getenv("Temp"))) != 0:
+                    print("[xpykg:error]: {} failed to install... maybe try later".format(package))
+                    exit(1)
+                else:
+                    print("[xpykg:sucess]: {} installed sucesfully".format(package))
+                    append_to_install(package, contents[package]['version'], contents[package]['remover'])
+                    exit(0)
+                    
+            elif contents[package]['source'].split('.')[len(contents[package].split('.'))-1] == 'msi':
+                with open("{}\\setup.msi".format(getenv("Temp")), 'wb') as setup:
+                    setup.write(exe_contents.content)
+                setup.close()
+
+                if system("{}\\setup.msi".format(getenv("Temp"))) != 0:
+                    print("[xpykg:error]: {} failed to install".format(package))
+                else:
+                    print("[xpykg:sucess]: {} instaled sucessfully".format(package))
+                    append_to_install(package, contents[package]['version'], contents[package]['remover'])
+                    exit(0)
+    else:
+        print("[xpykg:error]: package index not found.. maybe run sync")
+
+def append_to_install(pkgname: str, version: str, remover: str):
+    '''
+    append_to_install(pkgname, version, remover): append package to install database
+    '''
+    if isfile("C:\\Program Files\\xpykg\\installed-packages") == False:
+        with open("C:\\Program Files\\xpykg\\installed-packages", 'w') as db:
+            db.write("{} {} {}\n".format(pkgname, version, remover))
+        db.close()
+    else:
+        with open("C:\\Program Files\\xpykg\\installed-packages", 'a+') as db:
+            db.write("{} {} {}\n".format(pkgname, version, remover))
+        db.close()
+        
 if __name__ == "__main__":
     try:
         if argv[1] in ["-h", "help", "--help"]:
@@ -110,6 +175,9 @@ version: show xpkg version''')
             list_packages()
         elif argv[1] in ["-s", "search", "--search"] and len(argv) == 3:
             search_packages(argv[2])
+        elif argv[1] in ["-i", "install", "--install"]:
+            if len(argv) == 3:
+                install_package(argv[2])
         else:
             print("[xpykg:error]: invalid argument or not sufficient arguements")
     except IndexError:
